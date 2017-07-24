a761442aa9bdb96dca67d22a1687d7e3688be142
hbblzjy <hbblzjy@163.com>
2017-07-24 18:20:22 +0800

    Initial Commit

---
+++ SwiftGuideMengBanDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftGuideMengBanDemo
+//
+//  Created by JasonHao on 2017/7/24.
+//  Copyright © 2017年 JasonHao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
